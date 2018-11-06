84e186d9acc45c85ccf60607be914249a3ecc61f
nxf <15860035280@163.com>
2018-11-06 09:59:44 +0800

    nxf

---
+++ src/components/films.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>这里是影片</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+import index from '../components/index.vue'
+import films from '../components/films.vue'
+import mine from '../components/mine.vue'
+import movies from '../components/movies.vue'
+import outCard from '../components/outCard.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path:'/index',
+      name:'index',
+      component:index
+    },
+    {
+      path:'/films',
+      component:films,
+    },
+    {
+      path:'/mine',
+      component:mine
+    },
+    {
+      path:'/movies',
+      component:movies
+    },
+    {
+      path:'/outCard',
+      component:outCard
+    }
+  ]
+})
